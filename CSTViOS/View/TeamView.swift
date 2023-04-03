7c15b3eed2269304659fcd5a605a540b991d0a75
Fabio Lindemberg Pereira <fabiolindemberg@gmail.com>
2023-04-03 10:11:20 -0300

    Add TeamView

---
+++ CSTViOS/View/TeamView.swift
@@ -0,0 +1,37 @@
+//
+//  TeamView.swift
+//  CSTViOS
+//
+//  Created by Fabio Lindemberg on 03/04/23.
+//
+
+import SwiftUI
+
+struct TeamView: View {
+    var team: Team?
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            AsyncImage(url: URL(string: team?.imageUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                Circle()
+                    .foregroundColor(Color("placeholder-background"))
+
+            }
+            .frame(width: 60, height: 60)
+        
+            Text(team?.name ?? "")
+                .font(.caption)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct TeamView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamView(team: matchMock.teamTwo)
+    }
+}
