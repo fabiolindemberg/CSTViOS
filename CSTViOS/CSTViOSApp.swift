86585254bebab03a348a71fd6b83fa710f00dd74
Fabio Pereira <fabio_pereiradasilva@apple.com>
2023-04-02 23:46:16 -0300

    Initial Commit

---
+++ CSTViOS/CSTViOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTViOSApp.swift
+//  CSTViOS
+//
+//  Created by Fabio Lindemberg on 02/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CSTViOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
